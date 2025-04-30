9d4cdf01fb136766d77b0270568c1df5c41fa862
Eldiiar Bekbolotov <eldiiarbekbolotov@gmail.com>
2025-04-29 21:09:04 -0400

    UI Upgrade

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+// (c) 2025 Eldiiar Bekbolotov. Licensed under the MIT License.
 var scenes = document.getElementsByClassName("scene");
 var switchScene = function (index) {
   for (var i = 0; i < scenes.length; i++) {
@@ -14,15 +15,30 @@ document.getElementById("send-btn").addEventListener("click", sendMessage);
 document
   .getElementById("user-textarea")
   .addEventListener("keydown", function (event) {
-    // Check if the pressed key is ENTER and if SHIFT is not pressed (to allow new lines with SHIFT+ENTER)
     if (event.key === "Enter" && !event.shiftKey) {
-      event.preventDefault(); // Prevent a new line from being added
+      event.preventDefault();
       sendMessage();
     }
   });
 
+let timerInterval;
+let timer = 0;
+function startTimer() {
+  timerInterval = setInterval(function () {
+    timer += 0.01;
+    document.getElementById("timer").innerText = timer.toFixed(2);
+  }, 10);
+}
+function resetTimer() {
+  clearInterval(timerInterval);
+  timer = 0;
+  document.getElementById("timer").innerText = timer.toFixed(2);
+}
 function sendMessage() {
   document.getElementById("greeting").style.display = "none";
+  resetTimer();
+  startTimer();
+
   var userMessage = document.getElementById("user-textarea").value;
   if (!userMessage.trim()) return;
 
@@ -43,6 +59,7 @@ function sendMessage() {
     .then((response) => response.json())
     .then((data) => {
       document.getElementById("loading").style.display = "none";
+      resetTimer();
       if (data.response) {
         chatBox.innerHTML +=
           "<p><strong>EldiiarBot:</strong> " + data.response + "</p>";
@@ -59,6 +76,7 @@ function sendMessage() {
     })
     .catch((error) => {
       document.getElementById("loading").style.display = "none";
+      resetTimer();
       chatBox.innerHTML +=
         '<p><img src="/static/android-chrome-512x512.png" alt="EldiiarBot" class="logo-icon"/><strong>EldiiarBot:</strong> It seems that there has been an error trying to fetch the data you requested. Please try again later. Error type: ' +
         error.message +
